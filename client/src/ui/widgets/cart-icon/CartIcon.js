2d9c57cb5783f85ad0e6c5e95cfe29f8c2bcae45
ignacio <ignaciocieza@hotmail.com>
2019-12-18 18:28:05 -0300

    cartIcon cierra el menu-phone

--- client/src/ui/widgets/cart-icon/CartIcon.js
+++ client/src/ui/widgets/cart-icon/CartIcon.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { bindActionCreators } from "redux";
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
+import { toggleMenuHiddenToTrue } from '../../../api/actions/indexActions';
 import { selectCartItemsCount } from '../../../api/reducers/helperFunctions';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 // import './cartIcon.styles.scss';
 import useStyles from './cartIcon.styles.js';
 
-const CartIcon = ({ itemCount, history }) => {
+const CartIcon = ({ itemCount, history, toggleMenuHiddenToTrue }) => {
 
     const classes = useStyles();
 
@@ -19,7 +21,13 @@ const CartIcon = ({ itemCount, history }) => {
                 itemCount > 0 ? (<div className={classes.bubleCart}>
                     <span className={classes.bubleCartText} >{itemCount}</span>
                 </div>) : ""}
-                <ShoppingCartIcon className={classes.shoppingCartIcon} onClick={() => history.push('/checkout')} />
+                <ShoppingCartIcon
+                    className={classes.shoppingCartIcon}
+                    onClick={() => {
+                        history.push('/checkout');
+                        toggleMenuHiddenToTrue()
+                    }}
+                />
             </div>
         </React.Fragment>
     );
@@ -27,6 +35,10 @@ const CartIcon = ({ itemCount, history }) => {
 
 const mapStateToProps = createStructuredSelector({
     itemCount: selectCartItemsCount
-})
+});
 
-export default withRouter(connect(mapStateToProps, null)(CartIcon));
+const mapDispatchToProps = (dispatch) => ({
+    toggleMenuHiddenToTrue: bindActionCreators(toggleMenuHiddenToTrue, dispatch),
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(CartIcon));
